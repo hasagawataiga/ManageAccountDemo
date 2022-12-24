63f2456533c6c3a23fa5fc5b851daee5a429c5a5
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2022-12-24 21:43:26 +0100

    Initial commit

---
+++ app/src/main/java/orca/application/manageaccountdemo/UserLogin.java
@@ -0,0 +1,14 @@
+package orca.application.manageaccountdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_login);
+    }
+}
---
+++ app/src/main/java/orca/application/manageaccountdemo/UserRegister.java
@@ -0,0 +1,14 @@
+package orca.application.manageaccountdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_register);
+    }
+}
