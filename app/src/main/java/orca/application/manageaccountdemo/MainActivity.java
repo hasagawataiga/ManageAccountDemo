2611ff55d07bcfce9dec39209d4be047e1309927
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2022-12-27 15:56:10 +0100

    Add google and facebook login interface

--- app/src/main/java/orca/application/manageaccountdemo/MainActivity.java
+++ app/src/main/java/orca/application/manageaccountdemo/MainActivity.java
@@ -2,7 +2,9 @@ package orca.application.manageaccountdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToSignIn (View view){
+        Intent signInIntent = new Intent(this, UserLogin.class);
+        startActivity(signInIntent);
+    }
+
+    public void goToSignUp (View view){
+        Intent signUpIntent = new Intent(this, UserRegister.class);
+        startActivity(signUpIntent);
     }
 }
--- app/src/main/java/orca/application/manageaccountdemo/UserLogin.java
+++ app/src/main/java/orca/application/manageaccountdemo/UserLogin.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class UserLogin extends AppCompatActivity {
+import com.google.android.gms.common.SignInButton;
 
+public class UserLogin extends AppCompatActivity {
+    SignInButton signInButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
+        signInButton = findViewById(R.id.sign_in_button);
     }
 }
