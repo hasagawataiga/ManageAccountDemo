64fa34e235152c5da41d01429016023a8279d916
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2022-12-27 17:24:27 +0100

    fix bugs in Layout about Google and Facebook sign in button

--- app/src/main/java/orca/application/manageaccountdemo/UserLogin.java
+++ app/src/main/java/orca/application/manageaccountdemo/UserLogin.java
@@ -3,15 +3,16 @@ package orca.application.manageaccountdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.google.android.gms.common.SignInButton;
 
 public class UserLogin extends AppCompatActivity {
-    SignInButton signInButton;
+    Button btn_login_Google;
+    Button btn_login_Facebook;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
-        signInButton = findViewById(R.id.sign_in_button);
     }
 }
